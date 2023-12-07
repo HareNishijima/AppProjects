4b931c393b98f0eef1bcdf57fe647470dc3cb80e
hare <rehare1223@gmail.com>
2023-12-07 22:11:34 +0900

    Add comment

--- tic_tac_toe/src/App.jsx
+++ tic_tac_toe/src/App.jsx
@@ -5,16 +5,19 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [turnCount, setTurnCount] = useState(0);
   const currentSquares = history[turnCount];
-    // 手順が偶数の時はX、奇数の時はOのターン
-    const turn = turnCount % 2 === 0;
+  // 手順が偶数の時はX、奇数の時はOのターン
+  const turn = turnCount % 2 === 0;
 
   function handlePlay(nextSquares) {
+    // jumpToで過去のターンに戻った時、それ以降の内容はすべて削除される
     const nextHistory = [...history.slice(0, turnCount + 1), nextSquares];
     setHistory(nextHistory);
     setTurnCount(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
+    // turnCountが更新されるとcurrentSquaresが更新され
+    // 盤面がリセットされたように見える
     setTurnCount(nextMove);
   }
 
