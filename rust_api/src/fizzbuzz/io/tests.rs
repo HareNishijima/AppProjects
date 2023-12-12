cd98113d353216104149b40b10cbbee9236fba9a
hare <rehare1223@gmail.com>
2023-12-12 22:50:08 +0900

    Green 入力値が数値以外の時はエラーになる

--- rust_api/src/fizzbuzz/io/tests.rs
+++ rust_api/src/fizzbuzz/io/tests.rs
@@ -11,3 +11,15 @@ fn 入力値を数値にパースできる(){
 	// then
 	assert_eq!(res, 1);
 }
+
+#[test]
+fn 入力値が数値以外の時はエラーになる(){
+	// given
+	let input = "invalid";
+
+	// when
+	let res = parsei32(input);
+
+	// then
+	assert!(res.is_err());
+}
