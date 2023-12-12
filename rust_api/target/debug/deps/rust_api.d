2ecf17e9599faf03c0fb360892d7098299b6dc87
hare <rehare1223@gmail.com>
2023-12-12 21:57:07 +0900

    Init project

---
+++ rust_api/target/debug/deps/rust_api.d
@@ -0,0 +1,5 @@
+C:\Users\rehar\Desktop\Projects\rust_api\target\debug\deps\rust_api.exe: src\main.rs
+
+C:\Users\rehar\Desktop\Projects\rust_api\target\debug\deps\rust_api.d: src\main.rs
+
+src\main.rs:
---
+++ rust_api/target/debug/rust_api.d
@@ -0,0 +1,1 @@
+C:\Users\rehar\Desktop\Projects\rust_api\target\debug\rust_api.exe: C:\Users\rehar\Desktop\Projects\rust_api\src\main.rs
