0687ebf1bc2d8593bc419c5efcea93a4a9d84d5e
hare <rehare1223@gmail.com>
2023-12-12 22:44:47 +0900

    Green 入力値を出力する

--- rust_api/src/fizzbuzz/io.rs
+++ rust_api/src/fizzbuzz/io.rs
@@ -1,6 +1,12 @@
+use std::num::ParseIntError;
+
 #[cfg(test)]
 mod tests;
 
-pub fn input()->Result<i64, ()>{
-	todo!();
+pub fn input()->Result<i32, ParseIntError>{
+	let mut input = String::new();
+	std::io::stdin().read_line(&mut input).ok();
+	let res = input.trim().parse::<i32>()?;
+
+	Ok(res)
 }
