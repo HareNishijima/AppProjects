33edebbf29445543d215d1782cbda1203c38f2d4
hare <rehare1223@gmail.com>
2023-12-20 23:09:27 +0900

    WIP Add header

--- nextjs-crud/app/page.tsx
+++ nextjs-crud/app/page.tsx
@@ -1,7 +1,13 @@
+import { HomeIcon } from '@heroicons/react/24/outline'
+
 export default function Top() {
   return (
     <main>
+      <header className="flex bg-sky-200">
+        <HomeIcon className="h-6 w-6 text-white" />
+        <div>CRUD</div>
+      </header>
       hello world
     </main>
-  )
+  );
 }
