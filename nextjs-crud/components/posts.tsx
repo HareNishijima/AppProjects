a69d5564e061efbd0b64dc1cdebb7dec09ad743d
hare <rehare1223@gmail.com>
2024-01-11 20:36:39 +0900

    Add delete button

--- nextjs-crud/components/posts.tsx
+++ nextjs-crud/components/posts.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { PostData } from "../app/lib/data/post";
+import { TrashIcon } from "@heroicons/react/24/outline";
 
 type PropsData = {
   posts: PostData[];
@@ -12,12 +13,15 @@ export const Post = (props: PropsData) => {
     <ul>
       {posts?.map((post, idx) => (
         <div className="py-2" key={idx}>
-          <li className="flex bg-white border rounded p-2">
+          <li className="flex justify-between bg-white border rounded p-2">
             <div className="flex justify-center items-center h-14 w-14">
               <div className="text-4xl">{post.top}</div>
             </div>
-            <div className="flex">
+            <div className="flex grow">
               <div className="text-xl h-full whitespace-pre-wrap">{post.content}</div>
+            </div>
+            <div className="flex items-end">
+              <TrashIcon className="h-6 w-6 text-gray-400" />
             </div>
           </li>
         </div>
