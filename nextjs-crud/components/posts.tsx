17abf4890a26d13ccd2a24220cae01009048970d
hare <rehare1223@gmail.com>
2024-01-14 16:04:54 +0900

    Add edit icon

--- nextjs-crud/components/posts.tsx
+++ nextjs-crud/components/posts.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { PostData } from "../app/lib/data/post";
 import { TrashIcon } from "@heroicons/react/24/outline";
+import { PencilIcon } from "@heroicons/react/24/outline";
 
 type PropsData = {
   posts: PostData[];
@@ -36,7 +37,10 @@ export const Post = (props: PropsData) => {
             <div className="flex grow">
               <div className="text-xl h-full whitespace-pre-wrap">{post.content}</div>
             </div>
-            <div className="flex flex-col justify-end">
+            <div className="flex gap-x-2 items-end">
+              <button>
+                <PencilIcon className="h-6 w-6 text-gray-400" />
+              </button>
               <button onClick={() => handleDeleteButton(post.id)}>
                 <TrashIcon className="h-6 w-6 text-gray-400" />
               </button>
