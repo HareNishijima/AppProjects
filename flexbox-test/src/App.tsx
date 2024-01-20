4adf399a4549680aa42472b6b3d3ac2a7eae1f5a
hare <rehare1223@gmail.com>
2024-01-20 22:00:59 +0900

    Add justify sample

--- flexbox-test/src/App.tsx
+++ flexbox-test/src/App.tsx
@@ -1,8 +1,31 @@
 export const App = () => {
-
   return (
     <>
-      text
+      <div className="flex m-4 w-56 h-56 bg-red-100 rounded">
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+      </div>
+      <div className="flex m-4 w-56 h-56 bg-red-100 rounded justify-center">
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+      </div>
+      <div className="flex m-4 w-56 h-56 bg-red-100 rounded items-center">
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+      </div>
+      <div className="flex m-4 w-56 h-56 bg-red-100 rounded justify-center items-center">
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+        <div className="h-12 w-12 bg-red-500 rounded" />
+      </div>
+
     </>
-  )
+  );
 };
