5fa1796b467b8118c0f210b0041176d47646968d
hare <rehare1223@gmail.com>
2024-01-25 22:15:25 +0900

    Add grid

--- cook-bingo/src/App.tsx
+++ cook-bingo/src/App.tsx
@@ -1,8 +1,15 @@
 export const App = () => {
-
   return (
     <>
-      hello world
+      <div className="m-auto w-fit justify-items-center bg-blue-100">
+        <div className="grid gap-4 grid-cols-4 p-4">
+          {Array(16)
+            .fill(0)
+            .map((i) => (
+              <div className="w-28 h-28 bg-white"></div>
+            ))}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
