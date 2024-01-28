25e62d15c978c817d3fee79b2a3472f28c806653
hare <rehare1223@gmail.com>
2024-01-28 20:44:19 +0900

    Add reset button

--- cook-bingo/src/App.tsx
+++ cook-bingo/src/App.tsx
@@ -10,15 +10,26 @@ export const App = () => {
     setBingo(newBingo);
   };
 
+  const handleResetBingo = () => {
+    setBingo(Array(9).fill(0));
+  };
+
   return (
     <>
-      <div className="m-auto w-fit justify-items-center bg-blue-100">
-        <div className="grid gap-6 grid-cols-3 p-6 rounded-lg">
-          {Array(9)
-            .fill(0)
-            .map((n, i) => (
-              <Card key={i} index={i} item={bingo[i]} handleSetBingo={handleSetBingo}/>
-            ))}
+      <div className="w-full m-auto max-w-screen-md">
+        <div className="m-auto w-fit justify-items-center bg-blue-100">
+          <div className="grid gap-6 grid-cols-3 p-6 rounded-lg">
+            {Array(9)
+              .fill(0)
+              .map((n, i) => (
+                <Card key={i} index={i} item={bingo[i]} handleSetBingo={handleSetBingo} />
+              ))}
+          </div>
+        </div>
+        <div className="flex w-full justify-center mt-4">
+          <button className="w-1/4 h-8 shadow-md bg-orange-400 rounded" onClick={handleResetBingo}>
+            reset
+          </button>
         </div>
       </div>
     </>
