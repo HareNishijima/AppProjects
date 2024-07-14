aff6f91d7b8affd6f80b1c1e0840d28fd75053a7
hare <rehare1223@gmail.com>
2024-07-14 17:29:47 +0900

    またまたコード変更

--- VueProject/src/pages/index.vue
+++ VueProject/src/pages/index.vue
@@ -1,44 +1,23 @@
 <script setup="ts">
 import { ref } from "vue";
-const btn = ref < Boolean > true;
-const btn2 = ref < Boolean > true;
+const dialog = ref(false);
 </script>
 
 <template>
-  <v-container>
-    <v-btn ref="btn" prepend-icon="mdi-variable" width="204">
-      Ref Activator
-    </v-btn>
+  <div class="text-center pa-4">
+    <v-btn @click="dialog = true"> Open Dialog </v-btn>
 
-    <v-dialog :activator="btn" max-width="340">
-      <template v-slot:default="{ isActive }">
-        <v-card prepend-icon="mdi-variable" text="1">
-          <template v-slot:actions>
-            <v-btn
-              class="ml-auto"
-              text="Close"
-              @click="isActive.value = false"
-            ></v-btn>
-          </template>
-          <v-btn ref="btn2" prepend-icon="mdi-variable" width="204">
-            Ref Activator
-          </v-btn>
-
-          <v-dialog :activator="btn2" max-width="340">
-            <template v-slot:default="{ isActive }">
-              <v-card prepend-icon="mdi-variable" text="2">
-                <template v-slot:actions>
-                  <v-btn
-                    class="ml-auto"
-                    text="Close"
-                    @click="isActive.value = false"
-                  ></v-btn>
-                </template>
-              </v-card>
-            </template>
-          </v-dialog>
-        </v-card>
-      </template>
+    <v-dialog v-model="dialog" width="auto">
+      <v-card
+        max-width="400"
+        prepend-icon="mdi-update"
+        text="Your application will relaunch automatically after the update is complete."
+        title="Update in progress"
+      >
+        <template v-slot:actions>
+          <v-btn class="ms-auto" text="Ok" @click="dialog = false"></v-btn>
+        </template>
+      </v-card>
     </v-dialog>
-  </v-container>
+  </div>
 </template>
