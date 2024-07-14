23b3be7bdd3c6f212efc32714fade8e9b3662ebf
hare <rehare1223@gmail.com>
2024-07-14 15:47:07 +0900

    二重モーダル実装

--- VueProject/src/pages/index.vue
+++ VueProject/src/pages/index.vue
@@ -27,32 +27,35 @@ const dialog2 = ref < Boolean > false;
             <v-spacer></v-spacer>
 
             <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
-          </v-card-actions>
-        </v-card>
-      </template>
-    </v-dialog>
+            <v-dialog max-width="500">
+              <template v-slot:activator="{ props: activatorProps2 }">
+                <v-btn
+                  v-bind="activatorProps2"
+                  color="surface-variant"
+                  text="Open Dialog2"
+                  variant="flat"
+                ></v-btn>
+              </template>
 
-    <v-dialog max-width="500">
-      <template v-slot:activator="{ props: activatorProps2 }">
-        <v-btn
-          v-bind="activatorProps2"
-          color="surface-variant"
-          text="Open Dialog2"
-          variant="flat"
-        ></v-btn>
-      </template>
+              <template v-slot:default="{ isActive2 }">
+                <v-card title="Dialog2">
+                  <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </v-card-text>
 
-      <template v-slot:default="{ isActive2 }">
-        <v-card title="Dialog2">
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
 
-          <v-card-actions>
-            <v-spacer></v-spacer>
-
-            <v-btn text="Close Dialog" @click="isActive2.value = false"></v-btn>
+                    <v-btn
+                      text="Close Dialog"
+                      @click="isActive2.value = false"
+                    ></v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
           </v-card-actions>
         </v-card>
       </template>
