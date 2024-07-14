77ce9025d0ab7f4110b31dc208cbc448d3d31ceb
hare <rehare1223@gmail.com>
2024-07-14 16:32:21 +0900

    修正

--- VueProject/src/pages/index.vue
+++ VueProject/src/pages/index.vue
@@ -1,8 +1,4 @@
-<script setup="ts">
-import { ref } from "vue";
-const dialog = ref < Boolean > false;
-const dialog2 = ref < Boolean > false;
-</script>
+<script setup="ts"></script>
 
 <template>
   <div class="d-flex w-screen ga-4">
@@ -18,39 +14,32 @@ const dialog2 = ref < Boolean > false;
 
       <template v-slot:default="{ isActive }">
         <v-card title="Dialog">
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text>
+          <v-card-text> Dialog </v-card-text>
 
           <v-card-actions>
             <v-spacer></v-spacer>
 
             <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
             <v-dialog max-width="500">
-              <template v-slot:activator="{ props: activatorProps2 }">
+              <template v-slot:activator="{ props: activatorProps }">
                 <v-btn
-                  v-bind="activatorProps2"
+                  v-bind="activatorProps"
                   color="surface-variant"
                   text="Open Dialog2"
                   variant="flat"
                 ></v-btn>
               </template>
 
-              <template v-slot:default="{ isActive2 }">
+              <template v-slot:default="{ isActive }">
                 <v-card title="Dialog2">
-                  <v-card-text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </v-card-text>
+                  <v-card-text> Dialog2 </v-card-text>
 
                   <v-card-actions>
                     <v-spacer></v-spacer>
 
                     <v-btn
                       text="Close Dialog"
-                      @click="isActive2.value = false"
+                      @click="isActive.value = false"
                     ></v-btn>
                   </v-card-actions>
                 </v-card>
