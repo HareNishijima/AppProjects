27c664e61ce60f576f31d0f44099d9dbec67ff7e
hare <rehare1223@gmail.com>
2024-07-14 17:05:46 +0900

    二重モーダル

--- VueProject/src/pages/index.vue
+++ VueProject/src/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup="ts">
 import { ref } from "vue";
 const btn = ref < Boolean > true;
+const btn2 = ref < Boolean > true;
 </script>
 
 <template>
@@ -11,11 +12,7 @@ const btn = ref < Boolean > true;
 
     <v-dialog :activator="btn" max-width="340">
       <template v-slot:default="{ isActive }">
-        <v-card
-          prepend-icon="mdi-variable"
-          text="When using a ref, the dialog will bind its listeners to the ref element. This works for any element and custom components."
-          title="Ref Activator"
-        >
+        <v-card prepend-icon="mdi-variable" text="1">
           <template v-slot:actions>
             <v-btn
               class="ml-auto"
@@ -23,6 +20,23 @@ const btn = ref < Boolean > true;
               @click="isActive.value = false"
             ></v-btn>
           </template>
+          <v-btn ref="btn2" prepend-icon="mdi-variable" width="204">
+            Ref Activator
+          </v-btn>
+
+          <v-dialog :activator="btn2" max-width="340">
+            <template v-slot:default="{ isActive }">
+              <v-card prepend-icon="mdi-variable" text="2">
+                <template v-slot:actions>
+                  <v-btn
+                    class="ml-auto"
+                    text="Close"
+                    @click="isActive.value = false"
+                  ></v-btn>
+                </template>
+              </v-card>
+            </template>
+          </v-dialog>
         </v-card>
       </template>
     </v-dialog>
