0318434e38543d6ef943928f7ae44cc7e9e7d1f3
hare <rehare1223@gmail.com>
2024-07-14 15:45:02 +0900

    v-dialog2つ配置

--- VueProject/src/pages/index.vue
+++ VueProject/src/pages/index.vue
@@ -1,34 +1,61 @@
 <script setup="ts">
 import { ref } from "vue";
 const dialog = ref < Boolean > false;
+const dialog2 = ref < Boolean > false;
 </script>
 
 <template>
-  <v-dialog max-width="500">
-    <template v-slot:activator="{ props: activatorProps }">
-      <div class="h-screen w-screen d-flex justify-center align-center">
+  <div class="d-flex w-screen ga-4">
+    <v-dialog max-width="500">
+      <template v-slot:activator="{ props: activatorProps }">
         <v-btn
           v-bind="activatorProps"
           color="surface-variant"
           text="Open Dialog"
           variant="flat"
         ></v-btn>
-      </div>
-    </template>
+      </template>
 
-    <template v-slot:default="{ isActive }">
-      <v-card title="Dialog">
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </v-card-text>
+      <template v-slot:default="{ isActive }">
+        <v-card title="Dialog">
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
+          <v-card-actions>
+            <v-spacer></v-spacer>
 
-          <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
-        </v-card-actions>
-      </v-card>
-    </template>
-  </v-dialog>
+            <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+
+    <v-dialog max-width="500">
+      <template v-slot:activator="{ props: activatorProps2 }">
+        <v-btn
+          v-bind="activatorProps2"
+          color="surface-variant"
+          text="Open Dialog2"
+          variant="flat"
+        ></v-btn>
+      </template>
+
+      <template v-slot:default="{ isActive2 }">
+        <v-card title="Dialog">
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn text="Close Dialog" @click="isActive2.value = false"></v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+    </v-dialog>
+  </div>
 </template>
