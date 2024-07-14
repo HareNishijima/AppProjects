0d4ca1d576121b208857887a0c14db38ca955b9e
hare <rehare1223@gmail.com>
2024-07-14 14:31:18 +0900

    ダイアログ追加

--- VueProject/src/pages/index.vue
+++ VueProject/src/pages/index.vue
@@ -1,7 +1,34 @@
-<template>
-  <HelloWorld />
-</template>
-
-<script lang="ts" setup>
-  //
+<script setup="ts">
+import { ref } from "vue";
+const dialog = ref < Boolean > false;
 </script>
+
+<template>
+  <v-dialog max-width="500">
+    <template v-slot:activator="{ props: activatorProps }">
+      <div class="h-screen w-screen d-flex justify-center align-center">
+        <v-btn
+          v-bind="activatorProps"
+          color="surface-variant"
+          text="Open Dialog"
+          variant="flat"
+        ></v-btn>
+      </div>
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card title="Dialog">
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
